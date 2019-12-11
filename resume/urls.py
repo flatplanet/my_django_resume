bea42cbacfc2f47222f9eac46888ed6624627a79
John Elder <john@codemy.com>
2019-12-11 11:54:27 -0800

    removed about page

--- resume/urls.py
+++ resume/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name="home"),
-	path('about.html', views.about, name="about"),
+
 
 ]
--- resume/views.py
+++ resume/views.py
@@ -5,10 +5,7 @@ def home(request):
 	return render(request, 'home.html', {})
 
 
-def about(request):
-	f_name = "Bill"
-	l_name = "Elder"
-	return render(request, 'about.html', {'first_name': f_name, 'last_name': l_name})
+
 
 
 
